c6c9723e66107ea5b016141e2eb8eee352ff8b0d
Sunday Brian <b.sunday@platcorpgroup.com>
2024-07-26 01:42:25 +0300

    update: create service and controller files

--- gen.ts
+++ gen.ts
@@ -42,10 +42,18 @@ const generateDtosAndEntities = async () => {
         const modelFolderName = modelName.toLowerCase();
         const dtoFolderPath = path.join('src', modelFolderName, 'dto');
         const entityFolderPath = path.join('src', modelFolderName, 'entities');
+        const searchFolderPath = path.join('src', modelFolderName);
+        const serviceFolderPath = path.join('src', modelFolderName);
+        const controllerFolderPath = path.join('src', modelFolderName);
+        const moduleFolderPath = path.join('src', modelFolderName);
 
         // Ensure folders exist
         fs.mkdirSync(dtoFolderPath, { recursive: true });
         fs.mkdirSync(entityFolderPath, { recursive: true });
+        fs.mkdirSync(searchFolderPath, { recursive: true });
+        fs.mkdirSync(serviceFolderPath, { recursive: true });
+        fs.mkdirSync(controllerFolderPath, { recursive: true });
+        fs.mkdirSync(moduleFolderPath, { recursive: true });
 
         // Create source files for the DTOs
         const createDtoFile = project.createSourceFile(
@@ -162,10 +170,237 @@ const generateDtosAndEntities = async () => {
             moduleSpecifier: '@nestjs/swagger',
         });
 
+
+        // Service file creation
+        const serviceFile = project.createSourceFile(
+            path.join(serviceFolderPath, `${modelFolderName}.service.ts`),
+            {},
+            { overwrite: true }
+        );
+
+        serviceFile.addClass({
+            name: `${modelName}Service`,
+            isExported: true,
+            decorators: [{ name: 'Injectable', arguments: [] }],
+            ctors: [{
+                parameters: [
+                    { name: 'prisma', type: 'PrismaService', decorators: [{ name: 'Inject', arguments: ['PrismaService'] }] },
+                ],
+            }],
+            methods: [
+                {
+                    name: 'create',
+                    parameters: [
+                        { name: 'createDto', type: `Create${modelName}Dto` },
+                    ],
+                    statements: `return this.prisma.${modelFolderName}.create({ data: createDto });`,
+                },
+                {
+                    name: 'findAll',
+                    parameters: [
+                        { name: 'query', type: `${modelName}SearchQuery` },
+                    ],
+                    statements: `const filters: Prisma.${modelName}WhereInput = {};
+                const page = +query.page || 1;
+                const size = +query.limit || 10;
+
+                Object.keys(query).forEach(key => {
+                    if (!['limit', 'page'].includes(key)) {
+                        filters[key] = { equals: query[key] }
+                    }
+                });
+
+                const { take, skip } = pagination(page, size);
+                const items = await this.prisma.${modelFolderName}.findMany({
+                    where: filters,
+                    skip,
+                    take,
+                });
+
+                const count = await this.prisma.${modelFolderName}.count({ where: filters });
+                return getPagingData({ count, rows: items }, page, size);`,
+                },
+                {
+                    name: 'findOne',
+                    parameters: [
+                        { name: 'id', type: 'number' },
+                    ],
+                    statements: `return this.prisma.${modelFolderName}.findUnique({ where: { id } });`,
+                },
+                {
+                    name: 'update',
+                    parameters: [
+                        { name: 'id', type: 'number' },
+                        { name: 'updateDto', type: `Update${modelName}Dto` },
+                    ],
+                    statements: `return this.prisma.${modelFolderName}.update({ where: { id }, data: updateDto });`,
+                },
+                {
+                    name: 'remove',
+                    parameters: [
+                        { name: 'id', type: 'number' },
+                    ],
+                    statements: `return this.prisma.${modelFolderName}.delete({ where: { id } });`,
+                },
+            ],
+        });
+
+        serviceFile.addImportDeclaration({
+            namedImports: ['Injectable'],
+            moduleSpecifier: '@nestjs/common',
+        });
+        serviceFile.addImportDeclaration({
+            namedImports: ['PrismaService'],
+            moduleSpecifier: 'src/prisma/prisma.service',
+        });
+        serviceFile.addImportDeclaration({
+            namedImports: [`Create${modelName}Dto`],
+            moduleSpecifier: `./dto/create-${modelFolderName}.dto`,
+        });
+        serviceFile.addImportDeclaration({
+            namedImports: [`Update${modelName}Dto`],
+            moduleSpecifier: `./dto/update-${modelFolderName}.dto`,
+        });
+        serviceFile.addImportDeclaration({
+            namedImports: [`${modelName}SearchQuery`],
+            moduleSpecifier: `./entities/${modelFolderName}.search`,
+        });
+        serviceFile.addImportDeclaration({
+            namedImports: ['Prisma'],
+            moduleSpecifier: '@prisma/client',
+        });
+        serviceFile.addImportDeclaration({
+            namedImports: ['pagination', 'getPagingData'],
+            moduleSpecifier: 'src/shared/paginations',
+        });
+
+        // Controller file creation
+        const controllerFile = project.createSourceFile(
+            path.join(controllerFolderPath, `${modelFolderName}.controller.ts`),
+            {},
+            { overwrite: true }
+        );
+
+        controllerFile.addClass({
+            name: `${modelName}Controller`,
+            isExported: true,
+            decorators: [
+                { name: 'Controller', arguments: [`'${modelFolderName}'`] },
+                { name: 'ApiTags', arguments: [`'${modelFolderName}'`] },
+            ],
+            ctors: [{
+                parameters: [
+                    { name: `${modelFolderName}Service`, type: `${modelName}Service`, decorators: [{ name: 'Inject', arguments: [`${modelName}Service`] }] },
+                ],
+            }],
+            methods: [
+                {
+                    name: 'create',
+                    decorators: [{ name: 'Post', arguments: [] }],
+                    parameters: [
+                        { name: 'createDto', type: `Create${modelName}Dto`, decorators: [{ name: 'Body', arguments: [] }] },
+                    ],
+                    statements: `return this.${modelFolderName}Service.create(createDto);`,
+                },
+                {
+                    name: 'findAll',
+                    decorators: [{ name: 'Post', arguments: ['search'] }],
+                    parameters: [
+                        { name: 'query', type: `${modelName}SearchQuery`, decorators: [{ name: 'Body', arguments: [] }] },
+                    ],
+                    statements: `return this.${modelFolderName}Service.findAll(query);`,
+                },
+                {
+                    name: 'findOne',
+                    decorators: [{ name: 'Get', arguments: [':id'] }],
+                    parameters: [
+                        { name: 'id', type: 'string', decorators: [{ name: 'Param', arguments: ['id'] }] },
+                    ],
+                    statements: `return this.${modelFolderName}Service.findOne(+id);`,
+                },
+                {
+                    name: 'update',
+                    decorators: [{ name: 'Patch', arguments: [':id'] }],
+                    parameters: [
+                        { name: 'id', type: 'string', decorators: [{ name: 'Param', arguments: ['id'] }] },
+                        { name: 'updateDto', type: `Update${modelName}Dto`, decorators: [{ name: 'Body', arguments: [] }] },
+                    ],
+                    statements: `return this.${modelFolderName}Service.update(+id, updateDto);`,
+                },
+                {
+                    name: 'remove',
+                    decorators: [{ name: 'Delete', arguments: [':id'] }],
+                    parameters: [
+                        { name: 'id', type: 'string', decorators: [{ name: 'Param', arguments: ['id'] }] },
+                    ],
+                    statements: `return this.${modelFolderName}Service.remove(+id);`,
+                },
+            ],
+        });
+
+        controllerFile.addImportDeclaration({
+            namedImports: ['Controller', 'Post', 'Get', 'Patch', 'Delete', 'Body', 'Param'],
+            moduleSpecifier: '@nestjs/common',
+        });
+        controllerFile.addImportDeclaration({
+            namedImports: [`${modelName}Service`],
+            moduleSpecifier: `./${modelFolderName}.service`,
+        });
+        controllerFile.addImportDeclaration({
+            namedImports: [`Create${modelName}Dto`],
+            moduleSpecifier: `./dto/create-${modelFolderName}.dto`,
+        });
+        controllerFile.addImportDeclaration({
+            namedImports: [`Update${modelName}Dto`],
+            moduleSpecifier: `./dto/update-${modelFolderName}.dto`,
+        });
+        controllerFile.addImportDeclaration({
+            namedImports: [`${modelName}SearchQuery`],
+            moduleSpecifier: `./entities/${modelFolderName}.search`,
+        });
+
+        // Module file creation
+        const moduleFile = project.createSourceFile(
+            path.join(moduleFolderPath, `${modelFolderName}.module.ts`),
+            {},
+            { overwrite: true }
+        );
+
+        moduleFile.addClass({
+            name: `${modelName}Module`,
+            isExported: true,
+            decorators: [{
+                name: 'Module',
+                arguments: [`{
+                imports: [],
+                controllers: [${modelName}Controller],
+                providers: [${modelName}Service],
+            }`],
+            }],
+        });
+
+        moduleFile.addImportDeclaration({
+            namedImports: ['Module'],
+            moduleSpecifier: '@nestjs/common',
+        });
+        moduleFile.addImportDeclaration({
+            namedImports: [`${modelName}Service`],
+            moduleSpecifier: `./${modelFolderName}.service`,
+        });
+        moduleFile.addImportDeclaration({
+            namedImports: [`${modelName}Controller`],
+            moduleSpecifier: `./${modelFolderName}.controller`,
+        });
+
         // Save the source files
         createDtoFile.saveSync();
         updateDtoFile.saveSync();
         entityFile.saveSync();
+
+        serviceFile.saveSync();
+        controllerFile.saveSync();
+        moduleFile.saveSync();
+
     });
 };
 
---
+++ todo.md
@@ -0,0 +1,7 @@
+[] controllers endpoints add default swagger docs
+[] pascal case the prisma models in the service file
+[] export each service file
+[] accept params eg --modelname user
+instead of generating the entire db
+[] add newly created module to app module
+[] exclude ids and timestamps from dtos
